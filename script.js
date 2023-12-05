b9d6238ecea12bf0b27a28cd70c7ecdd98c85a62
SirIyanu <105286160+SirIyanu@users.noreply.github.com>
2023-12-05 09:25:22 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function printToConsole(n1, n2){
+    const parameters={
+        number1: n1,
+        number2: n2,
+    }
+
+    return parameters
+     
+
+}
+console.log(printToConsole(
+    1, 2
+))
+
+console.log(printToConsole(
+    6, 7
+))
